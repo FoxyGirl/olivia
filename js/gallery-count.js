88de0d74ca1418f9621d902489ca6725ae1e3b79
Elena Pavlova <lenlho22@gmail.com>
2016-06-09 16:34:53 +0300

    Try create resizing for Slider

--- js/gallery-count.js
+++ js/gallery-count.js
@@ -16,9 +16,8 @@
   var galleryWidth = gallery.clientWidth;
   console.log('galleryWidth = ' + galleryWidth);
   
-  var counIteration = Math.floor(galleryWidth/itemWidth);
-  console.log('counIteration = ' + counIteration);
-  
+ 
+   
   var itemСomputedStyle = getStyle(contentBlock[0]);
   var itemMargin = parseInt(itemСomputedStyle.marginLeft, 10);
   console.log('itemMargin = ' + itemMargin );
@@ -26,8 +25,46 @@
   var galleryWidth = gallery.clientWidth;
   console.log('galleryWidth = ' + galleryWidth );
   
+    
+  resizeSlider();
+  window.addEventListener('resize', resizeSlider);
+  
+  //перестроение слайдера
+  function resizeSlider() {
+    console.log('!!!!!!!!');
+    
+     //ширина доступного окна 
+  var windowWidth = document.documentElement.clientWidth;
+  console.log('windowWidth = ' + windowWidth);
+  
+  
+  var countLine = Math.floor(galleryWidth/itemWidth);
+  console.log('countLine = ' + countLine);
+    
+    countLine = ( windowWidth < 1196 ) ? 3 : 4;
+  console.log('countLine = ' + countLine);
+  
+  // спрятали лишние блоки
+  for (var i = countLine, length = contentBlock.length; i < length; i++ ) {
+    contentBlock[i].style.display = 'none';    
+  }
+    
   var controls = controlBlock[0].getElementsByTagName('i');
   console.log(controls.length);
+  
+  var countContr = Math.ceil(contentBlock.length / countLine); 
+    
+    if ( controls.length > countContr ) {
+      for (var i = countContr, length = controls.length; i < length; i++ ) {
+        controls[i].style.display = 'none';    
+      }
+    }
+
+    if ( controls.length < countContr ) {
+      var controlNew = document.createElement('i');
+      document.querySelector('.screenshorts-gallery-controls').appendChild(controlNew);
+    }
+  }
   
   //кросс-браузерное получение стилей элемента (elem)
   function getStyle(elem) {

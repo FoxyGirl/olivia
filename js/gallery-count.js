004e78e127a0a5d14cb3de998c6c2ce4167f50e3
Elena Pavlova <lenlho22@gmail.com>
2016-06-10 16:38:08 +0300

    Change count of items and controls

--- js/gallery-count.js
+++ js/gallery-count.js
@@ -3,20 +3,24 @@
   
   var gallery = document.getElementById('galleryCount');
   var contentBlock = gallery.getElementsByClassName('screenshorts-gallery-item');
-  var controlBlock = gallery.getElementsByClassName('screenshorts-gallery-controls');
-  console.log('items = ' + contentBlock.length );
+
   var item = contentBlock[0];
-  console.log('item ' + item.tagName);
+  //item = item.querySelector('img');
+  console.log('item ' + item.tagName);  
   
   //ширина 1-го элемента
   var itemWidth = item.clientWidth;
-  console.log('itemWidth = ' + itemWidth );
+  console.log('itemWidth = ' + itemWidth );  //??
   
+  // controls
+  var controlBlock = gallery.getElementsByClassName('screenshorts-gallery-controls');
+  console.log('items = ' + contentBlock.length );  
+  var controls = controlBlock[0].getElementsByTagName('i');
+  console.log('controls = ' + controls.length);
+    
   //ширина всего блока
   var galleryWidth = gallery.clientWidth;
-  console.log('galleryWidth = ' + galleryWidth);
-  
- 
+  console.log('galleryWidth = ' + galleryWidth); 
    
   var itemСomputedStyle = getStyle(contentBlock[0]);
   var itemMargin = parseInt(itemСomputedStyle.marginLeft, 10);
@@ -33,33 +37,37 @@
   function resizeSlider() {
     console.log('!!!!!!!!');
     
-     //ширина доступного окна 
-  var windowWidth = document.documentElement.clientWidth;
-  console.log('windowWidth = ' + windowWidth);
-  
-  
-  var countLine = Math.floor(galleryWidth/itemWidth);
-  console.log('countLine = ' + countLine);
-    
+    //ширина доступного окна 
+    var windowWidth = document.documentElement.clientWidth;
+    console.log('windowWidth = ' + windowWidth);
+
+
+    var countLine = Math.floor(galleryWidth/itemWidth);
+    console.log('countLine = ' + countLine);
+
     countLine = ( windowWidth < 1196 ) ? 3 : 4;
-  console.log('countLine = ' + countLine);
-  
-  // спрятали лишние блоки
-  for (var i = countLine, length = contentBlock.length; i < length; i++ ) {
-    contentBlock[i].style.display = 'none';    
-  }
+    console.log('countLine = ' + countLine);
     
-  var controls = controlBlock[0].getElementsByTagName('i');
-  console.log(controls.length);
-  
-  var countContr = Math.ceil(contentBlock.length / countLine); 
+    // показали нужные блоки
+    for (var i = 0; i < countLine; i++ ) {
+      contentBlock[i].style.display = '';    
+    }
+
+    // спрятали лишние блоки
+    for (var i = countLine, length = contentBlock.length; i < length; i++ ) {
+      contentBlock[i].style.display = 'none';    
+    }
+
+    
+    var countContr = Math.ceil(contentBlock.length / countLine); 
     
     if ( controls.length > countContr ) {
       for (var i = countContr, length = controls.length; i < length; i++ ) {
         controls[i].style.display = 'none';    
       }
     }
-
+    
+    //добавить недостающе контролы
     if ( controls.length < countContr ) {
       var controlNew = document.createElement('i');
       document.querySelector('.screenshorts-gallery-controls').appendChild(controlNew);

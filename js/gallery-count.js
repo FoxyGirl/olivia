2478bc067fa5a3a36b8077c05347ded52201e840
Elena Pavlova <lenlho22@gmail.com>
2016-06-14 13:48:50 +0300

    Create auto count of items in line

--- js/gallery-count.js
+++ js/gallery-count.js
@@ -4,32 +4,23 @@
   var gallery = document.getElementById('galleryCount');
   var contentBlock = gallery.getElementsByClassName('screenshorts-gallery-item');
 
-  var item = contentBlock[0];
-  //item = item.querySelector('img');
-  console.log('item ' + item.tagName);  
-  
-  //ширина 1-го элемента
-  var itemWidth = item.clientWidth;
-  console.log('itemWidth = ' + itemWidth );  //??
-  
   // controls
-  var controlBlock = gallery.getElementsByClassName('screenshorts-gallery-controls');
-  console.log('items = ' + contentBlock.length );  
-  var controls = controlBlock[0].getElementsByTagName('i');
+  var controlBlock = document.getElementById('scrGalControls');
+  var controls = controlBlock.getElementsByTagName('i');
   console.log('controls = ' + controls.length);
     
-  //ширина всего блока
-  var galleryWidth = gallery.clientWidth;
-  console.log('galleryWidth = ' + galleryWidth); 
-   
+  var item = contentBlock[0];  
+  //ширина 1-го элемента
+  var itemWidth = item.clientWidth;
+  console.log('itemWidth = ' + itemWidth );  //??  
+ 
   var itemСomputedStyle = getStyle(contentBlock[0]);
   var itemMargin = parseInt(itemСomputedStyle.marginLeft, 10);
   console.log('itemMargin = ' + itemMargin );
   
-  var galleryWidth = gallery.clientWidth;
-  console.log('galleryWidth = ' + galleryWidth );
-  
-    
+  //ширина всего блока 1-го элемента
+  itemWidth += 2*itemMargin;
+  console.log('itemWidth = ' + itemWidth );
   resizeSlider();
   window.addEventListener('resize', resizeSlider);
   
@@ -40,14 +31,18 @@
     //ширина доступного окна 
     var windowWidth = document.documentElement.clientWidth;
     console.log('windowWidth = ' + windowWidth);
-
+    
+    //ширина галлереи
+    var galleryWidth = gallery.clientWidth;
+    console.log('galleryWidth = ' + galleryWidth );
+    galleryWidth += 2*itemMargin;
 
     var countLine = Math.floor(galleryWidth/itemWidth);
     console.log('countLine = ' + countLine);
-
+/*
     countLine = ( windowWidth < 1196 ) ? 3 : 4;
     console.log('countLine = ' + countLine);
-    
+ */   
     // показали нужные блоки
     for (var i = 0; i < countLine; i++ ) {
       contentBlock[i].style.display = '';    
@@ -63,10 +58,10 @@
     console.log('countContr = ' + countContr);
         
     //добавить недостающие контролы
-    if ( controls.length < countContr ) {
+    while ( controls.length < countContr ) {
       var controlNew = document.createElement('i');
       controlNew.setAttribute('data-toggler', (controls.length + 1));
-      document.querySelector('.screenshorts-gallery-controls').appendChild(controlNew);
+      controlBlock.appendChild(controlNew);
     }
     
     // показали нужные контролы
@@ -79,14 +74,16 @@
       controls[i].style.display = 'none';    
     }
     
-
-    /* //убра
-    if ( controls.length > countContr ) {
-      for (var i = countContr, length = controls.length; i < length; i++ ) {
-        controls[i].style.display = 'none';    
+  }
+  
+  document.querySelector('.screenshorts-gallery-controls').addEventListener('click', changeSlider);
+  
+  function changeSlider(e) {
+    var targetElem = e.target;
+    if (targetElem.tagName != 'I')  
+      {return; } else {
+        console.log("!! " + targetElem.getAttribute('data-toggler'));
       }
-    }     
-    */
   }
   
   

0e3038f06cde84e4f6d9462953b82a8f9b1ce269
Elena Pavlova <lenlho22@gmail.com>
2016-07-06 14:47:47 +0300

    Create arrow for window scrolling to up

--- js/arrow-to-up.js
+++ js/arrow-to-up.js
@@ -1,12 +1,13 @@
 (function () {
   "use strict";
   
-  /* Scrolling Window*/
-  var el = document.getElementById('toTop');	
-  //если окно проскроллировано на 50px, показываем кнопку для прокрутки наверх
-  el.style.display = (window.pageYOffset > '50' ? 'block' : 'none');		
+  /* Window Scrolling */
+  var HEIGHT_SHOW = 50;  
+  var el = document.getElementById('toUp');
+  //if window scrolling more than HEIGHT_SHOW show anchor to up
+  el.style.display = (window.pageYOffset > HEIGHT_SHOW ? 'block' : 'none');		
   window.onscroll = function () {
-      el.style.display = (window.pageYOffset > '50' ? 'block' : 'none');
+      el.style.display = (window.pageYOffset > HEIGHT_SHOW ? 'block' : 'none');
   }	
   //обработчик клика по кнопки прокрутки
   el.addEventListener('click', function(e) {
@@ -14,12 +15,12 @@
       toUp();
   }, false);
 
-  //функция прокрутки окна наверх
+  //function window scrolling
   function toUp() {
-      //переменная скорость тем больше, чем больше промотали вниз
-      var speed = window.pageYOffset/10;
-      window.scrollBy(0,-speed);
-      if (window.pageYOffset > 0) {requestAnimationFrame(toUp);}
-      }		
+    //переменная скорость тем больше, чем больше промотали вниз
+    var speed = window.pageYOffset/10;
+    window.scrollBy(0,-speed);
+    if (window.pageYOffset > 0) {requestAnimationFrame(toUp);}
+  }	
   
 })();
